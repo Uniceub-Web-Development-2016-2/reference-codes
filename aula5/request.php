2068a09c35873ea071c30988a22bf26c3bd6d055
uniceub <caio.cmsilva@gmail.com>
2016-09-20 18:24:29 -0300

    aula 5

---
+++ aula5/request.php
@@ -0,0 +1,71 @@
+<?php
+class Request{
+        private $method;
+        private $protocol;
+        private $server_ip;
+        private $remote_ip;
+        private $resource;
+        private $params;
+	private $body;
+  
+	 public function __construct($method, $protocol, $serverAddress, $clientAddress, $path, $queryString, $body)
+    {
+        $this->method = $method;
+        $this->protocol = $protocol;
+        $this->server_ip = $serverAddress;
+        $this->remote_ip = $clientAddress;
+        $this->resource = $path;
+        $this->setParams($queryString);
+        $this->body = $body;
+    }
+		 
+        public function toString(){
+                $request = "";
+                $Inc = 1;
+                foreach($this->params as $param) {
+                        $request .= "P".$Inc."=".$param."&amp";
+                        $Inc++;
+        }
+        return $this->protocol.'://'.$this->server_ip.'/'.$this->resource.'?'.$request;
+        }
+        public function setMethod($method){
+                $this->method = $method;
+        }
+        public function getMethod(){
+                return $this->method;
+        }
+        public function setProtocol($protocol){
+               $this->protocol = $protocol;
+        }
+        public function getProtocol(){
+                return $this->protocol;
+        }
+        public function setServer_IP($server_ip){
+                $this->server_ip = $server_ip;
+        }
+        public function getServer_IP(){
+                return $this->server_ip;
+        }
+         public function setRemote_IP($Remote_ip){
+                $this->remote_ip = $remote_ip;
+        }
+        public function getRemote_IP(){
+                return $this->remote_ip;
+        }
+        public function setResource($resource){
+                $this->resource = $resource;
+        }
+        public function getResource(){
+                return $this->resource;
+        }
+        
+	public function setParams($paramsString)
+    	{
+        	parse_str($paramsString, $paramsArray);
+       		 $this->params = $paramsArray;
+    	}	
+
+        public function getParameters(){
+                return $this->params;
+        }
+}
---
+++ aula5/resource_controller.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once ('request.php');
+
+class ResourceController
+{	
+ 	private $METHODMAP = ['GET' => 'search' , 'POST' => 'create' , 'PUT' => 'update', 'DELETE' => 'remove' ];
+	
+	public function treat_request($request) {
+		return $this->{$this->METHODMAP[$request->getMethod()]}($request);
+	
+	}
+
+	private function search($request) {
+		$query = 'SELECT * FROM'.$request->getResource().'WHERE'.self::queryParams($request->getParameters());
+		return $query; 
+	}
+		
+	private function queryParams($params) {
+		$query = "";		
+		foreach($params as $key => $value) {
+			$query .= $key.' = '.$value.' AND ';	
+		}
+		$query = substr($query,0,-5);
+		return $query;
+	}
+
+	
+}
+
+
+
+
