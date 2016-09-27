1e21996c87ade7b31b8f94b3bf03837af63c1b53
uniceub <caio.cmsilva@gmail.com>
2016-09-27 17:27:55 -0300

    Database connector

---
+++ aula6/db_manager.php
@@ -0,0 +1,39 @@
+<?php
+
+class DBConnector extends PDO {
+   
+    private $engine;
+    private $host;
+    private $database;
+    private $user;
+    private $pass;
+    private $connector;	
+   
+    public function __construct(){
+        $this->engine = 'mysql';
+        $this->host = 'localhost';
+        $this->database = 'aula';
+        $this->user = 'root';
+        $this->pass = '';
+        $dns = $this->engine.':dbname='.$this->database.";host=".$this->host;
+        parent::__construct( $dns, $this->user, $this->pass );
+    }	
+		
+}
+
+$result = (new DBConnector())->query('SELECT * FROM user');
+var_dump($result->fetchAll());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
